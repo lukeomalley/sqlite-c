29d237c856981a36c438a38bd6bf0f3cb69b8975
Luke O'Malley <Lukeomalley95@gmail.com>
2020-07-31 22:27:59 -0400

    setup in-memory append only single table database

--- main.c
+++ main.c
@@ -7,6 +7,22 @@
 #include <string.h>
 #include <unistd.h>
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+#define TABLE_MAX_PAGES 100
+
+typedef struct
+{
+  uint32_t num_rows;
+  void *pages[TABLE_MAX_PAGES];
+} Table;
+
+typedef enum
+{
+  EXECUTE_SUCCESS,
+  EXECUTE_TABLE_FULL
+} ExecuteResult;
+
 typedef enum
 {
   META_COMMAND_SUCCESS,
@@ -16,7 +32,8 @@ typedef enum
 typedef enum
 {
   PREPARE_SUCCESS,
-  PREPARE_UNRECGONIZED_STATEMENT
+  PREPARE_UNRECGONIZED_STATEMENT,
+  PREPARE_SYNTAX_ERROR
 } PrepareResult;
 
 typedef enum
@@ -27,8 +44,36 @@ typedef enum
 
 typedef struct
 {
+  uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+typedef struct
+{
   StatementType type;
+  Row row_to_insert; // only used by insert statement
 } Statement;
+
+// HARDCODED TABLE
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// TABLE STRUCTURE
+const uint32_t PAGE_SIZE = 4096;
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+void print_row(Row *row)
+{
+  printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
 
 typedef struct
 {
@@ -36,6 +81,35 @@ typedef struct
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+void seralize_row(Row *source, void *destination)
+{
+  memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void *source, Row *destination)
+{
+  memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void *row_slot(Table *table, uint32_t row_num)
+{
+  uint32_t page_num = row_num / ROWS_PER_PAGE;
+  void *page = table->pages[page_num];
+  if (page == NULL)
+  {
+    // Allocate memory only when we try to access page
+    page = table->pages[page_num] = malloc(PAGE_SIZE);
+  }
+
+  uint32_t row_offset = row_num % ROWS_PER_PAGE;
+  uint32_t byte_offset = row_offset * ROW_SIZE;
+  return page + byte_offset;
+}
 
 MetaCommandResult do_meta_command(InputBuffer *input_buffer)
 {
@@ -54,6 +128,8 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
   if (strncmp(input_buffer->buffer, "insert", 6) == 0)
   {
     statement->type = STATEMENT_INSERT;
+    int args_assigned = sscanf(
+        input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
     return PREPARE_SUCCESS;
   }
 
@@ -66,17 +142,63 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
   return PREPARE_UNRECGONIZED_STATEMENT;
 }
 
-void execute_statement(Statement *statement)
+ExecuteResult execute_insert(Statement *statement, Table *table)
+{
+  if (table->num_rows >= TABLE_MAX_ROWS)
+  {
+    return EXECUTE_TABLE_FULL;
+  }
+
+  Row *row_to_insert = &(statement->row_to_insert);
+
+  seralize_row(row_to_insert, row_slot(table, table->num_rows));
+  table->num_rows += 1;
+
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_select(Statement *statement, Table *table)
+{
+  Row row;
+  for (uint32_t i = 0; i < table->num_rows; i++)
+  {
+    deserialize_row(row_slot(table, i), &row);
+    print_row(&row);
+  }
+
+  return EXECUTE_SUCCESS;
+}
+
+ExecuteResult execute_statement(Statement *statement, Table *table)
 {
   switch (statement->type)
   {
   case (STATEMENT_INSERT):
-    printf("This is where we would do an insert.\n");
-    break;
+    return execute_insert(statement, table);
   case (STATEMENT_SELECT):
-    printf("This is where we would do a select.\n");
-    break;
+    return execute_select(statement, table);
   }
+}
+
+Table *new_table()
+{
+  Table *table = malloc(sizeof(Table));
+  table->num_rows = 0;
+  for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++)
+  {
+    table->pages[i] = NULL;
+  }
+
+  return table;
+}
+
+void free_table(Table *table)
+{
+  for (int i = 0; table->pages[i]; i++)
+  {
+    free(table->pages[i]);
+  }
+  free(table);
 }
 
 InputBuffer *new_input_buffer()
@@ -118,6 +240,7 @@ ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 
 int main(int argc, char *argv[])
 {
+  Table *table = new_table();
   InputBuffer *input_buffer = new_input_buffer();
   while (true)
   {
@@ -141,12 +264,22 @@ int main(int argc, char *argv[])
     {
     case (PREPARE_SUCCESS):
       break;
+    case (PREPARE_SYNTAX_ERROR):
+      printf("Syntax error. Could not parse statement.\n");
+      continue;
     case (PREPARE_UNRECGONIZED_STATEMENT):
       printf("Unrecgonized keyword at start of '%s'.\n", input_buffer->buffer);
       continue;
     }
 
-    execute_statement(&statement);
-    printf("Executed.\n");
+    switch (execute_statement(&statement, table))
+    {
+    case (EXECUTE_SUCCESS):
+      printf("Executed.\n");
+      break;
+    case (EXECUTE_TABLE_FULL):
+      printf("Error: Table full.\n");
+      break;
+    }
   }
 }
