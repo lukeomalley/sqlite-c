fe7acfa582670229598a3c7052f39e7f3c3af7f3
Luke O'Malley <Lukeomalley95@gmail.com>
2020-07-31 21:01:30 -0400

    setup basics of virtual machine

--- main.c
+++ main.c
@@ -7,12 +7,77 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef enum
+{
+  META_COMMAND_SUCCESS,
+  META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum
+{
+  PREPARE_SUCCESS,
+  PREPARE_UNRECGONIZED_STATEMENT
+} PrepareResult;
+
+typedef enum
+{
+  STATEMENT_INSERT,
+  STATEMENT_SELECT
+} StatementType;
+
+typedef struct
+{
+  StatementType type;
+} Statement;
+
 typedef struct
 {
   char *buffer;
   size_t buffer_length;
   ssize_t input_length;
 } InputBuffer;
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+  if (strcmp(input_buffer->buffer, ".exit") == 0)
+  {
+    exit(EXIT_SUCCESS);
+  }
+  else
+  {
+    return META_COMMAND_UNRECOGNIZED_COMMAND;
+  }
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+  if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+  {
+    statement->type = STATEMENT_INSERT;
+    return PREPARE_SUCCESS;
+  }
+
+  if (strcmp(input_buffer->buffer, "select") == 0)
+  {
+    statement->type = STATEMENT_SELECT;
+    return PREPARE_SUCCESS;
+  }
+
+  return PREPARE_UNRECGONIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+  switch (statement->type)
+  {
+  case (STATEMENT_INSERT):
+    printf("This is where we would do an insert.\n");
+    break;
+  case (STATEMENT_SELECT):
+    printf("This is where we would do a select.\n");
+    break;
+  }
+}
 
 InputBuffer *new_input_buffer()
 {
@@ -58,14 +123,30 @@ int main(int argc, char *argv[])
   {
     print_prompt();
     read_input(input_buffer);
-    if (strcmp(input_buffer->buffer, ".exit") == 0)
+
+    if (input_buffer->buffer[0] == '.')
     {
-      close_input_buffer(input_buffer);
-      exit(EXIT_SUCCESS);
+      switch (do_meta_command(input_buffer))
+      {
+      case (META_COMMAND_SUCCESS):
+        continue;
+      case (META_COMMAND_UNRECOGNIZED_COMMAND):
+        printf("Unrecgonized command '%s\n", input_buffer->buffer);
+        continue;
+      }
     }
-    else
+
+    Statement statement;
+    switch (prepare_statement(input_buffer, &statement))
     {
-      printf("Unrecgonized command '%s'. \n", input_buffer->buffer);
+    case (PREPARE_SUCCESS):
+      break;
+    case (PREPARE_UNRECGONIZED_STATEMENT):
+      printf("Unrecgonized keyword at start of '%s'.\n", input_buffer->buffer);
+      continue;
     }
+
+    execute_statement(&statement);
+    printf("Executed.\n");
   }
 }
